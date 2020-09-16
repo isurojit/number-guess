78e66f38fb16fefa88b866da705fd64b4fa41e36
isurojit <iamsurojit@hotmail.com>
2020-09-16 09:48:04 +0530

    final commit

--- app.js
+++ app.js
@@ -11,16 +11,17 @@ Game Function:
 //Game Values
 let min = 1, 
     max= 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 //UI elements
-const UIgame = document.querySelector('#gmae'),
+const UIgame = document.querySelector('#game'),
       UIminNum = document.querySelector('.min-num'),
       UImaxNum = document.querySelector('.max-num'),
       UIguessBtn = document.querySelector('#guess-btn'),
       UIguessInput = document.querySelector('#guess-input'),
-      UIimage = document.querySelector('.win-img'),
+      UIimageWin = document.querySelector('.win-img'),
+      UIimagelose = document.querySelector('.lose-img'),
       UImessage = document.querySelector('.message');
 
 //Assign UI min and Max
@@ -28,32 +29,74 @@ UIminNum.textContent = min;
 UImaxNum.textContent = max;
 
 //Disable Ui image
-UIimage.style.display = 'none';
+UIimageWin.style.display = 'none';
+UIimagelose.style.display = 'none';
+
+//Play again event listner
+UIgame.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again')
+    window.location.reload();
+});
 
 //Listen for guess
 UIguessBtn.addEventListener('click', function(){
     let guess = parseInt(UIguessInput.value);
-console.log(guess);
 
     //Validate
     if(isNaN(guess) || guess < min || guess > max){
         setMessage(`Please enter a number between ${min} and ${max}`, 'red');
-    }
+    }else{
 
     //Check if won
     if(guess === winningNum){
-        //Disable Input
-        UIguessInput.disabled = true;
-        //Change Border
-        UIguessInput.style.borderColor = 'green';
-        //Set Message
-        setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+        gameOver(true,`${winningNum} is correct, YOU WIN!`);
+        UIimageWin.style.display = 'block';
 
-        UIimage.style.display = 'block';
     }else{
+        //Wrong Number
+        guessesLeft -= 1;
 
+        if(guessesLeft === 0){
+            //Game Over Lost
+            gameOver(false,`Game Over, YOU LOST! The correct number was ${winningNum}.`);
+            UIimagelose.style.display = 'block';
+        }else{
+
+            //Chnage Border Color
+            UIguessInput.style.borderColor = 'red';
+
+            //clear Input
+            UIguessInput.value = '';
+
+            //Game Continues - answer wrong
+            setMessage(`Wrong Guess!, You have ${guessesLeft} guesses left`,'red');
+        }
     }
+}
 });
+
+//Game Over
+function gameOver(won, msg){
+    let color;
+    won === true ? color = 'green' : color ='red';
+    //Disable Input
+    UIguessInput.disabled = true;
+    //Change Border
+    UIguessInput.style.borderColor = color;
+    //Set text color 
+    UImessage.style.color = color;
+    //Set Message
+    setMessage(msg);
+
+    //Play again
+    UIguessBtn.value = 'Play Again';
+    UIguessBtn. className += 'play-again';
+}
+
+//Get Winning NUm
+function getRandomNum(min, max){
+    return (Math.floor(Math.random()*(max-min+1)+min));
+}
 
 //Set message
 function setMessage(msg, color){
